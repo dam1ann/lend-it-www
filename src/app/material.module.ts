885f53823591393e55fa4afde6c4e88564e1e4d3
Damian Nurzyński <damiannnurzynski@gmail.com>
2017-10-02 20:19:21 +0200

    add simple layout

---
+++ src/app/material.module.ts
@@ -0,0 +1,67 @@
+import {NgModule} from "@angular/core";
+import {
+  CompatibilityModule, MdAutocompleteModule, MdButtonModule, MdButtonToggleModule, MdCardModule, MdCheckboxModule,
+  MdChipsModule,
+  MdDialogModule,
+  MdGridListModule, MdIconModule,
+  MdInputModule,
+  MdListModule,
+  MdMenuModule,
+  MdProgressBarModule,
+  MdProgressSpinnerModule,
+  MdRadioModule,
+  MdRippleModule,
+  MdSelectModule,
+  MdSidenavModule,
+  MdSliderModule,
+  MdSlideToggleModule,
+  MdSnackBarModule, MdTabsModule, MdToolbarModule, MdTooltipModule, StyleModule
+} from "@angular/material";
+import {OverlayModule} from "@angular/cdk/overlay";
+import {PortalModule} from "@angular/cdk/portal";
+import {PlatformModule} from "@angular/cdk/platform";
+
+const materialModules = [
+  MdAutocompleteModule,
+  MdButtonModule,
+  MdButtonToggleModule,
+  MdCardModule,
+  MdChipsModule,
+  MdCheckboxModule,
+  MdDialogModule,
+  MdGridListModule,
+  MdIconModule,
+  MdInputModule,
+  MdListModule,
+  MdMenuModule,
+  MdProgressBarModule,
+  MdProgressSpinnerModule,
+  MdRadioModule,
+  MdRippleModule,
+  MdSelectModule,
+  MdSidenavModule,
+  MdSliderModule,
+  MdSlideToggleModule,
+  MdSnackBarModule,
+  MdTabsModule,
+  MdToolbarModule,
+  MdTooltipModule,
+  OverlayModule,
+  PortalModule,
+  PlatformModule,
+  CompatibilityModule,
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    materialModules
+  ],
+  exports: [
+    materialModules
+  ]
+})
+
+export class MaterialModules{
+
+}
