6a8c0046035b097a49478e83f0dc0fbee04bcbaa
Damian Nurzyński <damiannnurzynski@gmail.com>
2017-10-11 20:30:27 +0200

    improve routing

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,38 +1,39 @@
 import {NgModule} from "@angular/core";
-import {UIRouterModule, UIRouter} from "@uirouter/angular";
+import {UIRouterModule} from "@uirouter/angular";
 import {LoginComponent} from "./login/login.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
 
 const states = [
-    {
-        name: 'hello',
-        url: '/login',
-        component: LoginComponent
-    }, {
-        name: 'login',
-        url: '/login',
-        component: LoginComponent
-    }, {
-        name: 'dashboard',
-        url: '/dashboard',
-        component: DashboardComponent
-    }
+  {
+    name: 'hello',
+    url: '/login',
+    component: LoginComponent
+  }, {
+    name: 'login',
+    url: '/login',
+    component: LoginComponent
+  }, {
+    name: 'dashboard',
+    url: '/dashboard',
+    component: DashboardComponent
+  }
 ];
 
 
 @NgModule({
-    imports: [
-        UIRouterModule.forRoot({
-            states: states,
-            useHash: false,
-            // config: function (router: UIRouter) {
-                // return router.urlService.rules.otherwise('/dashboard');
-            // }
-        })
-    ],
-    exports: [
-        UIRouterModule
-    ]
+  imports: [
+    UIRouterModule.forRoot({
+      otherwise: '/login',
+      states: states,
+      useHash: false,
+      // config: function (router: UIRouter) {
+      // return router.urlService.rules.otherwise('/dashboard');
+      // }
+    })
+  ],
+  exports: [
+    UIRouterModule
+  ]
 })
 
 export class RoutingModule {
