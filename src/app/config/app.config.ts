70698d6e0a86d8b5dc569897c7533ef6a57f0a63
Damian Nurzyński <damiannnurzynski@gmail.com>
2017-12-04 09:14:23 +0200

    change api url

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -19,11 +19,11 @@ export const APP_CONFIGURATION: AppConfig = {
     client_id: '1_x6s9gwd49dcs8o0g0kcocgkw4s84kkkwcg8c0sokwc4ssc8g4',
     grant_type: 'password',
     client_secret: '3wy05mtrii0wog4oo0woc4ogs80co0ckg4so48s4c4w4g0g8c4',
-    auth_url: 'https://powerful-meadow-36630.herokuapp.com/app_dev.php/oauth/v2/token',
+    auth_url: 'https://len-it-api.herokuapp.com/app_dev.php/oauth/v2/token',
     urls: {
-        me: 'https://powerful-meadow-36630.herokuapp.com/app_dev.php/api/me',
-        registry: 'https://powerful-meadow-36630.herokuapp.com/app_dev.php/api/registry',
-        movies: 'https://powerful-meadow-36630.herokuapp.com/app_dev.php/api/movies',
-        singleMovie: 'https://powerful-meadow-36630.herokuapp.com/app_dev.php/api/movie/'
+        me: 'https://lend-it-api.herokuapp.com/app_dev.php/api/me',
+        registry: 'https://lend-it-api.herokuapp.com/app_dev.php/api/registry',
+        movies: 'https://lend-it-api.herokuapp.com/app_dev.php/api/movies',
+        singleMovie: 'https://lend-it-api.herokuapp.com/app_dev.php/api/movie/'
     }
 };
