f78be2dddc3432c5993b2d450320bbfe61f8a9d8
Damian Nurzyński <damiannnurzynski@gmail.com>
2017-12-10 15:45:43 +0100

    fix auth bad url

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -20,9 +20,9 @@ export const APP_CONFIGURATION: AppConfig = {
     client_id: '1_15cdcd5bbnxckw0kos04s4ccwcsk80c044kwsg0ksgog4s0c0g',
     grant_type: 'password',
     client_secret: 'n2x28twxu0gowwgg0kcwoscccwscso4oww4ossoc0wc0wss8c',
-    auth_url: 'https://lend-it-api.herokuapp.com//oauth/v2/token',
+    auth_url: 'https://lend-it-api.herokuapp.com/oauth/v2/token',
     urls: {
-        me: 'https://lend-it-api.herokuapp.com/app_dev.php/api/me',
+        me: 'https://lend-it-api.herokuapp.com/api/me',
         registry: 'https://lend-it-api.herokuapp.com/api/registry',
         movies: 'https://lend-it-api.herokuapp.com/api/movies',
         singleMovie: 'https://lend-it-api.herokuapp.com/api/movie/',
